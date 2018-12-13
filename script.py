861efb7c1cc92e143d017c4d01f11e8513dba085
Bharathi-Kannan Dinakaran <bharathi-kannan.dinakaran@non.keysight.com>
2018-12-13 13:10:39 +0530

    few changes have been made

--- script.py
+++ script.py
@@ -25,10 +25,11 @@ def transform_data():
     parent_element = None
     try:
         for x in my_list:
+            tag_count = len(x.partition(' ')[0]) + 1
             if x.startswith("* "):
                 h2_count = 1
                 h1 = etree.SubElement(root, "h1")
-                h1.set("text_val", x[2:])
+                h1.set("text_val", x[tag_count:])
                 h1.set("index", str(h1_count))
                 parent_element = h1
                 h1_count += 1
@@ -36,43 +37,43 @@ def transform_data():
                 h3_count = 1
                 h2 = etree.SubElement(h1, "h2")
                 parent_element = h2
-                h2.set("text_val", x[3:])
-                h2.set("index", "%s.%s" %(h1.get("index"), str(h2_count)))
+                h2.set("text_val",x[tag_count:])
+                h2.set("index","%s.%s" %(h1.get("index"), str(h2_count)))
                 h2_count += 1
             elif x.startswith("*** "):
                 h4_count = 1
                 h3 = etree.SubElement(h2, "h3")
-                h3.set("text_val", x[4:])
+                h3.set("text_val",x[tag_count:])
                 h3.set("index", "%s.%s" % (h2.get("index"), str(h3_count)))
                 parent_element = h3
                 h3_count += 1
             elif x.startswith("**** "):
                 h4 = etree.SubElement(h3, "h4")
-                h4.set("text_val", x[5:])
+                h4.set("text_val",x[tag_count:])
                 h4.set("index", "%s.%s" % (h3.get("index"), str(h4_count)))
                 parent_element = h4
                 h4_count += 1
             elif x.startswith(". "):
                 ul1 = etree.SubElement(parent_element, "ul1")
-                ul1.set("text_val", x[2:])
+                ul1.set("text_val", x[tag_count:])
                 ul1.set("index", str(ul_count))
-                ul1.set("indent", "  -")
+                ul1.set("indent", " " * tag_count+ "-")
                 ul_parent_element = ul1
                 ul_count += 1
             elif x.startswith(".. "):
                 ul1.set("indent", "  +")
                 ul2 = etree.SubElement(ul1, "ul2")
-                ul2.set("text_val", x[3:])
+                ul2.set("text_val", x[tag_count:])
                 ul2.set("index", str(ul_count))
-                ul2.set("indent", "   -")
+                ul2.set("indent", " " * tag_count+ "-")
                 ul_parent_element = ul2
                 ul_count += 1
             elif x.startswith("... "):
-                ul2.set("indent", "   +")
+                ul2.set("indent","   +")
                 ul3 = etree.SubElement(ul2, "ul3")
-                ul3.set("text_val", x[4:])
+                ul3.set("text_val", x[tag_count:])
                 ul3.set("index", str(ul_count))
-                ul3.set("indent", "    -")
+                ul3.set("indent", " " * tag_count+ "-")
                 ul_parent_element = ul3
                 ul_count += 1
             else:
