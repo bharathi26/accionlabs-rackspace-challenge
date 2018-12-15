ab48eb34b1214915db19f4d1d94f06ce516c9183
Bharathi-Kannan Dinakaran <bharathi-kannan.dinakaran@non.keysight.com>
2018-12-15 19:16:32 +0530

    changes have been made for testing scenario

--- script.py
+++ script.py
@@ -6,6 +6,7 @@
 # * lib dependencies   :   pip install lxml 
 #/***********************************************************************/
 import sys
+from collections import defaultdict
 try:
     from lxml import etree
 except ImportError:
@@ -13,72 +14,38 @@ except ImportError:
 
 root = etree.Element("html")
 root.set('version', '1.0.1')
+tag_dict = defaultdict(lambda: 1)
 processedLines = []
 
+
 def transform_data():
-    my_list = [line.strip() for line in sys.stdin if len(line.strip()) != 0]
-    # with open(inFile, 'r') as f:
-    #     my_list = [line.strip() for line in f if len(line.strip()) != 0]
+    line_list = [line.strip() for line in sys.stdin if len(line.strip()) != 0]
 
     # start transforming the data
-    h1_count, h2_count, h3_count, h4_count, ul_count = (1,0,0,0,0)
-    parent_element = None
+    parent_element = root
+    ul_parent_element = None
     try:
-        for x in my_list:
-            tag_count = len(x.partition(' ')[0]) + 1
-            if x.startswith("* "):
-                h2_count = 1
-                h1 = etree.SubElement(root, "h1")
-                h1.set("text_val", x[tag_count:])
-                h1.set("index", str(h1_count))
-                parent_element = h1
-                h1_count += 1
-            elif x.startswith("** "):
-                h3_count = 1
-                h2 = etree.SubElement(h1, "h2")
-                parent_element = h2
-                h2.set("text_val",x[tag_count:])
-                h2.set("index","%s.%s" %(h1.get("index"), str(h2_count)))
-                h2_count += 1
-            elif x.startswith("*** "):
-                h4_count = 1
-                h3 = etree.SubElement(h2, "h3")
-                h3.set("text_val",x[tag_count:])
-                h3.set("index", "%s.%s" % (h2.get("index"), str(h3_count)))
-                parent_element = h3
-                h3_count += 1
-            elif x.startswith("**** "):
-                h4 = etree.SubElement(h3, "h4")
-                h4.set("text_val",x[tag_count:])
-                h4.set("index", "%s.%s" % (h3.get("index"), str(h4_count)))
-                parent_element = h4
-                h4_count += 1
-            elif x.startswith(". "):
-                ul1 = etree.SubElement(parent_element, "ul1")
-                ul1.set("text_val", x[tag_count:])
-                ul1.set("index", str(ul_count))
-                ul1.set("indent", " " * tag_count+ "-")
-                ul_parent_element = ul1
-                ul_count += 1
-            elif x.startswith(".. "):
-                ul1.set("indent", "  +")
-                ul2 = etree.SubElement(ul1, "ul2")
-                ul2.set("text_val", x[tag_count:])
-                ul2.set("index", str(ul_count))
-                ul2.set("indent", " " * tag_count+ "-")
-                ul_parent_element = ul2
-                ul_count += 1
-            elif x.startswith("... "):
-                ul2.set("indent","   +")
-                ul3 = etree.SubElement(ul2, "ul3")
-                ul3.set("text_val", x[tag_count:])
-                ul3.set("index", str(ul_count))
-                ul3.set("indent", " " * tag_count+ "-")
-                ul_parent_element = ul3
-                ul_count += 1
+        for line in line_list:
+            tag_len = len(line.partition(' ')[0])
+            if line.startswith("*"):
+                p_element = root if tag_len == 1 else tag_dict["ph%s" % (tag_len-1)]
+                h1 = etree.SubElement(p_element, "h%s" % tag_len)
+                h1.set("text_val", line[tag_len + 1:])
+                index = str(tag_dict["h%s" % tag_len]) if tag_len == 1 else "%s.%s" % \
+                         (p_element.get("index"), str(tag_dict["h%s" % tag_len]))
+                h1.set("index", index)
+                parent_element, tag_dict["ph%s" % tag_len] = h1, h1
+                tag_dict["h%s" % tag_len] += 1
+            elif line.startswith("."):
+                p_element = parent_element if tag_len == 1 else tag_dict["pu%s" % (tag_len -1)]
+                ul1 = etree.SubElement(p_element, "ul%s" % tag_len)
+                ul1.set("text_val", line[tag_len + 1:])
+                ul1.set("indent", " " * (tag_len + 1) + "-")
+                ul_parent_element, tag_dict["pu%s" % tag_len] = ul1, ul1
+                tag_dict["ul%s" % tag_len] += 1
             else:
-                update_value = ul_parent_element.get("text_val")+'\n'+'    '+x
-                ul_parent_element.set("text_val",update_value)
+                update_value = ul_parent_element.get("text_val") + '\n' + '    ' + line
+                ul_parent_element.set("text_val", update_value)
 
     except Exception as e:
         print("Error: %s." % (str(e)))
@@ -88,10 +55,12 @@ def load_data():
     try:
         for elt in root.iter():
             if elt is not root:
-                if elt.tag in ["h1", "h2", "h3", "h4"]:
+                if elt.tag.startswith("h"):
                     processedLines.append("%s %s" % (elt.get("index"), elt.get("text_val")))
                 else:
+                    elt.set("indent", elt.get("indent").replace("-", "+" if len(elt) else "-" ))
                     processedLines.append("%s %s" % (elt.get("indent", "-"), elt.get("text_val")))
+
     except Exception as e:
         print("Error: %s." % (str(e)))
 
